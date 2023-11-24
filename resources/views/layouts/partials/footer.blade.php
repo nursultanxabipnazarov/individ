5cee5363ab7ac82008778928b893f055d0dac323
Nursultan <nursultanxabipnazarov@gmail.com>
2023-11-24 11:21:22 +0500

    first individ

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      Developed By Nursultan
+    </div>
+    <strong>&copy; <?php echo date('Y')?> - </strong> - 
+  </footer>
